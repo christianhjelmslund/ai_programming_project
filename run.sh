be6434cdf757cd9e79d5939c43d3aace89b71bd9
s164399, Thomas Heshe <s164399@student.dtu.dk>
2020-05-21 09:29:51 +0200

    added levels

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-LEVELS_DIR=levels/new_levels/
-LEVEL=MAIIOO.lvl # change level name
+LEVELS_DIR=levels/comp20/
+LEVEL=MABaguettes.lvl # change level name
 
 G=150 # number of graphical steps - if you want only to run it in terminal just remove "-g $G" argument
 T=180 # time out in seconds
