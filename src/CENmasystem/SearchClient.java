7ce7fab1380bfb9a7a8265d5ee1dc40e34912ba7
s164399, Thomas Heshe <s164399@student.dtu.dk>
2020-05-22 09:24:36 +0200

    changed prints

--- src/CENmasystem/SearchClient.java
+++ src/CENmasystem/SearchClient.java
@@ -27,7 +27,7 @@ public class SearchClient {
 
             State leafState = bestFirstStrategy.getAndRemoveLeaf();
 
-            if (iterations == 100) {
+            if (iterations == 1000) {
 
 
                 System.err.println(bestFirstStrategy.searchStatus());
